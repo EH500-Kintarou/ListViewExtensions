9fdd2081e7f5b9acb4ca1e8b54fcbb7cc93490b6
EH500_Kintarou <EH500_Kintarou@example.com>
2024-08-24 18:51:05 +0900

    ver.1.0.1を.NET Coreプロジェクトに移植

--- ListViewExtensions/XmlDefinitions.cs
+++
@@ -1,6 +0,0 @@
-﻿using System.Windows.Markup;
-
-[assembly: XmlnsDefinition("http://schemas.eh500-kintarou.com/ListViewExtensions", "ListViewExtensions.Views.Actions")]
-[assembly: XmlnsDefinition("http://schemas.eh500-kintarou.com/ListViewExtensions", "ListViewExtensions.Views.Controls")]
-[assembly: XmlnsDefinition("http://schemas.eh500-kintarou.com/ListViewExtensions", "ListViewExtensions.Views.Converters")]
-[assembly: XmlnsDefinition("http://schemas.eh500-kintarou.com/ListViewExtensions", "ListViewExtensions.Views.AttachedProperties")]
