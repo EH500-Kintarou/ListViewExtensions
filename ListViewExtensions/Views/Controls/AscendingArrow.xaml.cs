2d5c6d15e9deeb0429b399c78a5d5cc4ddeaaac9
EH500_Kintarou <EH500_Kintarou@example.com>
2024-08-31 16:05:52 +0900

    SortableGridViewColumnHeaderの矢印をAscendingArrowとDescendingArrowに分離した（StyleでTemplateを置き換えられるように）

---
+++ ListViewExtensions/Views/Controls/AscendingArrow.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace ListViewExtensions.Views.Controls
+{
+	public class AscendingArrow : Control
+	{
+		static AscendingArrow()
+		{
+			DefaultStyleKeyProperty.OverrideMetadata(typeof(AscendingArrow), new FrameworkPropertyMetadata(typeof(AscendingArrow)));
+		}
+	}
+}
---
+++ ListViewExtensions/Views/Controls/DescendingArrow.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace ListViewExtensions.Views.Controls
+{
+	public class DescendingArrow : Control
+	{
+		static DescendingArrow()
+		{
+			DefaultStyleKeyProperty.OverrideMetadata(typeof(DescendingArrow), new FrameworkPropertyMetadata(typeof(DescendingArrow)));
+		}
+	}
+}
